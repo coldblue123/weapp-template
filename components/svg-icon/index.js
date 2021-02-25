8f5356cf380d3682a21c47baff0315f6894985f1
秦小野 <coldblue123@163.com>
2021-02-25 17:09:30 +0800

    feat: 增加iconfont 示例

--- components/svg-icon/index.js
+++ components/svg-icon/index.js
@@ -3,14 +3,17 @@ Component({
     styleIsolation: 'shared'
   },
   properties: {
+    // 图标名称
     icon: {
       type: String,
       value: ''
     },
+    // 图标颜色
     color: {
       type: String,
       value: ''
     },
+    // 图标尺寸rpx
     size: {
       type: String,
       value: ''
